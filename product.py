fa19463b1347e223a54f96cf6aeba2775331a304
Plwangg <plwanggg@gmail.com>
2019-09-14 14:31:26 +0800

    new version

--- product.py
+++ product.py
@@ -1,11 +1,33 @@
+import os
 product = []
+if os.path.isfile('product.csv'):
+	print('新增內容')
+	with open('product.csv', 'r') as file:
+		for line in file:
+			name , price = line.strip().split(',')
+			product.append([name, price])
+else:
+	print('新增第一筆資料')
+	with open('product.csv', 'w') as file:
+		file.write('商品,價格')
+	with open('product.csv', 'r') as file:
+		for line in file:
+			name , price = line.strip().split(',')
+			product.append([name, price])
+
+#user input
 while True:
 	name = input('請輸入商品名稱')
 	if name == 'q':
 		break
 	price = input('請輸入商品價格')
-	item = [] #--
-	item.append(name) #--
-	item.append(price) #--
-	product.append(item) #product,append([name, price])
+	item = [] 
+	item.append(name)
+	item.append(price)
+	product.append(item)
 print(product)
+
+#write file	
+with open('product.csv', 'w') as file:
+	for i in product:
+		file.write(i[0] + ',' + i[1] + '\n')
